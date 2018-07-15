710f69131dbcdc21a059699d044af8fe7c79fe41
Aaron <aaron.m.stricker@gmail.com>
2018-07-15 15:02:23 -0500

    restyle Devices

--- frontend/src/js/components/Devices.js
+++ frontend/src/js/components/Devices.js
@@ -2,8 +2,46 @@ import React, { Component } from "react";
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
-import { Container, List, Icon, Card, Popup, Grid } from 'semantic-ui-react';
+import { Container, List, Icon, Card, Popup } from 'semantic-ui-react';
 import moment from 'moment';
+
+const itemW = 250;
+const gutter = 5;
+const padding = 8;
+const boxShadow = '1px 1px 5px #b1b1b1';
+const Grid = styled.div`
+  align-self: flex-start;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+`;
+const GridHead = styled.div`
+  width: 100%;
+  background: white;
+  box-shadow: ${boxShadow};
+  margin: ${gutter}px;
+  padding: ${padding}px;
+`;
+const GridItem = styled.div`
+  width: ${itemW}px;
+  margin: ${gutter}px;
+  padding: ${padding}px;
+  background: white;
+  box-shadow: ${boxShadow};
+  & hr {
+    border-width: 0.5px;
+    border-style: solid;
+    border-color: #dcdcdc;
+    margin: 3px -${padding}px 3px;
+  }
+  & .extra {
+    font-size: 0.8em;
+  }
+  & .seen {
+    font-size: 0.8em;
+    color: #9a9a9a;
+  }
+`;
 
 const SPOOF_STATUS = gql`
   query spoofStatus {
@@ -25,11 +63,6 @@ const SPOOF_STATUS = gql`
   }
 `;
 
-const SpoofStatusWrap = styled.div`
-  background: white;
-  padding: 8px;
-`;
-
 const SpoofStatus = () => (
   <Query query={SPOOF_STATUS} pollInterval={5000}>
     {({ loading, error, data }) => {
@@ -38,7 +71,7 @@ const SpoofStatus = () => (
       let ping = data.spoofStatus.pingSweep;
       let scan = data.spoofStatus.portScan;
       return (
-        <SpoofStatusWrap>
+        <GridHead>
           <div>
             <Icon name='target' disabled={!ping.processing}/>
             {ping.processing ? 
@@ -53,7 +86,7 @@ const SpoofStatus = () => (
               `last portscan took ${scan.scanTime||0}ms` 
             }
           </div>
-        </SpoofStatusWrap>
+        </GridHead>
       )
     }}
   </Query>
@@ -74,27 +107,6 @@ const DEVICES = gql`
   }
 `;
 
-const Devices = () => (
-  <Query query={DEVICES} pollInterval={5000}>
-    {({ loading, error, data }) => {
-      if (loading) return "Loading...";
-      if (error) return `Error! ${error.message}`;
-
-      return (
-        <Grid>
-          <Grid.Column width={16}>
-            <SpoofStatus />
-          </Grid.Column>
-            {data.devices.map(d =>
-              <Device key={d.mac} {...d} />
-            )}
-            <div style={{clear: 'both'}} >&nbsp;</div>
-        </Grid>
-      );
-    }}
-  </Query>
-);
-
 function latestIp(ips) {
   return ips.reduce((latest, ip) => {
     if (Date.parse(latest.seen) - Date.parse(ip.seen) > 0) {
@@ -105,34 +117,42 @@ function latestIp(ips) {
   });
 }
 
-const Device = ({mac, ips, vendor, isSensor, isGateway}) => (
-  <Grid.Column mobile={16} tablet={8} computer={4} largeScreen={4} widescreen={2}>
-    <Card fluid style={{ height: '100%' }}>
-      <Card.Content>
-        <Card.Header>
-          {isSensor && 
-            <Popup trigger={<Icon name='eye' style={{float:'right'}} />} content='pi-net-mon sensor' />
-          }
-          {isGateway && 
-            <Popup trigger={<Icon name='hdd' style={{float: 'right'}} />} content='gateway' />
-          }
-          {mac}
-        </Card.Header>
-        <Card.Meta>
-          <span>{latestIp(ips).ip}</span>
-        </Card.Meta>
-        <Card.Description>{vendor || '(no vendor discovered)'}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <a>
-          <Icon name='clock' />
-          { moment(latestIp(ips).seen).from(new Date()) }
-        </a>
-      </Card.Content>
-    </Card>
-  </Grid.Column>
+const Devices = () => (
+  <Query query={DEVICES} pollInterval={5000}>
+    {({ loading, error, data }) => {
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <Grid>
+          <SpoofStatus />
+          {data.devices.map(d =><Device key={d.mac} {...d} />)}
+        </Grid>
+      );
+    }}
+  </Query>
 );
 
-
+const Device = ({mac, ips, vendor, isSensor, isGateway}) => (
+  <GridItem>
+    <div className='mac'>
+      {isSensor && 
+        <Popup trigger={<Icon name='eye' style={{float:'right'}} />} content='pi-net-mon sensor' />
+      }
+      {isGateway && 
+        <Popup trigger={<Icon name='hdd' style={{float: 'right'}} />} content='gateway' />
+      }
+      {mac}
+    </div>
+    <div className='ip'>{latestIp(ips).ip}</div>
+    <hr />
+    <div className='extra'>{vendor || '(no vendor discovered)'}</div>
+    <hr />
+    <div className='seen'>
+      <Icon name='clock' />
+      { moment(latestIp(ips).seen).from(new Date()) }
+    </div>
+  </GridItem>
+);
 
 export default Devices;
