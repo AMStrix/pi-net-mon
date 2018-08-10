0f7dd48358943a6440b0d35bcd783e87810e889f
Aaron <aaron.m.stricker@gmail.com>
2018-08-10 11:17:56 -0500

    better init sequence

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,24 @@ const gql = require('./gql');
 const l = require('./log');
 const broalyzer = require('./broalyzer');
 const spoof = require('./spoof');
+const feeds = require('./feeds');
 
 const app = express();
 
-broalyzer.init();
+broalyzer.init()
+  .then(feeds.init)
+  .then(init);
 
-app.use(session);
-app.use((req, res, next) => {
-  if (req.session.x) { req.session.x = req.session.x + 1; }
-  else { req.session.x = 1; }
-  next();
-})
-app.use('/graphql', gql);
-app.listen(4000, () => l.info('GraphQL started on localhost:4000/graphql'));
-
+function init() {
+  app.use(session);
+  app.use((req, res, next) => {
+    if (req.session.x) { req.session.x = req.session.x + 1; }
+    else { req.session.x = 1; }
+    next();
+  })
+  app.use('/graphql', gql);
+  app.listen(4000, () => l.info('GraphQL started on localhost:4000/graphql'));
+}
 
 function handleExit(signal) {
   l.info(`*** pi-net-mon shutting down (${signal}) ***`);
